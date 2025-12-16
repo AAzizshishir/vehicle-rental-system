e71c0bd30f3233e08c0350b2041657efda433f59
shishir <mdabdulaziz3745@gmail.com>
2025-12-16 18:10:04 +0600

    created tables for database

--- src/config/db.ts
+++ src/config/db.ts
@@ -13,8 +13,31 @@ const initDB = async () => {
         email VARCHAR(150) UNIQUE NOT NULL,
         password TEXT NOT NULL,
         phone VARCHAR(15),
-        role VARCHAR(50) NOT NULL,
-        created_at TIMESTAMP DEFAULT NOW()
+        role VARCHAR(50) NOT NULL CHECK (role IN ('admin', 'customer')),
+        created_at TIMESTAMP DEFAULT NOW())
+        `);
+
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS vehicles(
+        id SERIAL PRIMARY KEY,
+        vehicle_name VARCHAR(100) NOT NULL,
+        type VARCHAR(50) NOT NULL CHECK( type IN ('car', 'bike', 'van', 'SUV')),
+        registration_number VARCHAR(50) UNIQUE NOT NULL,
+        daily_rent_price NUMERIC(10,2) NOT NULL CHECK (daily_rent_price > 0),
+        availability_status VARCHAR(20) NOT NULL CHECK (availability_status IN ('available', 'booked'))
+        )
+        `);
+
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS bookings(
+        id SERIAL PRIMARY KEY,
+        customer_id INT REFERENCES users(id) ON DELETE CASCADE,
+        vehicle_id INT REFERENCES vehicles(id) ON DELETE CASCADE,
+        rent_start_date TIMESTAMP NOT NULL,
+        rent_end_date TIMESTAMP NOT NULL,
+        total_price NUMERIC(10,2) NOT NULL CHECK(total_price > 0),
+        status VARCHAR(20) NOT NULL CHECK (status IN ('active', 'cancelled', 'returned')),
+        CHECK (rent_end_date > rent_start_date)
         )
         `);
 };
