ae741fd8d67c2b021b2b4276d183ed0e4e50d26a
shishir <mdabdulaziz3745@gmail.com>
2025-12-16 17:03:39 +0600

    connect with postgreSQL database

---
+++ src/config/db.ts
@@ -0,0 +1,22 @@
+import { Pool } from "pg";
+import config from ".";
+
+export const pool = new Pool({
+  connectionString: `${config.connectionString}`,
+});
+
+const initDB = async () => {
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        phone VARCHAR(15),
+        role VARCHAR(50) NOT NULL,
+        created_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+};
+
+export default initDB;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express from "express";
+import config from "./config";
+import initDB from "./config/db";
+
 const app = express();
-const port = 5000;
+const port = config.port;
+
+// Initialized Database
+initDB();
 
 app.get("/", (req, res) => {
   res.send("Vehicle Rental System!");
